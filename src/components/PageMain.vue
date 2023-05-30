50ac70142e168800423772b2b8dc374bf0a2fa31
Alex Nunzi <alex.nunzi@hotmail.it>
2023-05-30 16:50:23 +0200

    Installati Axios, Bootstrap e sass. Creati components PageHeader e PageMain.

---
+++ src/components/PageMain.vue
@@ -0,0 +1,23 @@
+<template>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="text-center border border-black p-5">
+                    Contenuto
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: 'PageMain'
+    }
+</script>
+
+<style>
+
+</style>
