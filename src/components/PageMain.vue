852435929035a5c303f557a7f5e4a0da257b05ed
Alex Nunzi <alex.nunzi@hotmail.it>
2023-05-30 17:54:46 +0200

    Milestone 5: Creato component ProjectCard utilizzato per stampare a schermo le informazioni relative a ogni progetto.

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -1,31 +1,56 @@
 <template>
-    <main>
+    <main class="my-5">
         <div class="container">
-            <div class="row">
-
+            <div v-if="projects.length > 1" class="row">
+                <div v-for="project in projects" class="col-4 p-3">
+                    <ProjectCard 
+                    :baseUrl="this.baseUrl"
+                    :projectImg="project.preview_img"
+                    :placeholderImg="`${this.baseUrl}/img/h9pqRmsIwC1KOxfYbxgyvAFotT7SuEuNHayFtPir.png`"
+                    :projectTitle="project.title"
+                    :startDate="project.start_date"
+                    :endDate="project.end_date"
+                    :description="truncateText(project.description)"
+                    />
+                </div>
             </div>
+            <h4 v-else class="text-center">Non sono ancora presenti progetti da mostrare</h4>
         </div>
     </main>
 </template>
 
 <script>
+
+    import ProjectCard from './ProjectCard.vue';
+
     import axios from 'axios';
 
     export default {
         name: 'PageMain',
+        components: {
+            ProjectCard
+        },
         data() {
             return {
                 projects: [],
-                baseUrl: 'http://localhost:8000'
+                baseUrl: 'http://localhost:8000',
+                contentMaxLength: 150
             }
         },
         methods: {
             getProjects() {
                 axios.get(`${this.baseUrl}/api/projects`)
                 .then(response => {
-                    console.log(response.data);
+                    this.projects = response.data.results;
+                    console.log(response.data.results);
                 });
             },
+            truncateText(text) {
+                if (text && text.length > this.contentMaxLength) {
+                    return text.substr(0, this.contentMaxLength) + '...';
+                }
+                return text;
+            }
         },
         mounted() {
             this.getProjects();
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,34 @@
+<template>
+        <div class="card">
+            <img v-if="projectImg" :src="`${this.baseUrl}/storage/${this.projectImg}`" class="card-img-top">
+            <img v-else :src="placeholderImg" class="card-img-top"/>
+            <div class="card-body">
+                <h5 class="card-title">{{projectTitle}}</h5>
+                <h6>Data di inizio progetto: {{ startDate }}</h6>
+                <h6>Data di fine progetto: {{ endDate }}</h6>
+                <p>{{ description }}</p>
+            </div>
+        </div>
+</template>
+
+<script>
+ 
+ export default {
+    name: 'ProjectCard',
+    props: {
+        baseUrl: String,
+        projectImg: String,
+        projectTitle: String,
+        placeholderImg: String,
+        startDate: String,
+        endDate: String,
+        description: String
+
+    }
+ }
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
